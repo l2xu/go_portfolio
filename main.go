49e18303d557a10c77e9de90dc179112d06b010e
l2xu <weihrauch.business@gmail.com>
2023-01-05 14:48:36 +0100

    implemented db reading and rendering of page

--- main.go
+++ main.go
@@ -1,17 +1,26 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"html/template"
 	"io"
-	"io/ioutil"
+
+	// "io/ioutil"
 	"log"
 	"net/http"
-	"os"
-	"path/filepath"
 
-	"github.com/russross/blackfriday/v2"
+	// "os"
+	"path/filepath"
+	"time"
+
+	// "github.com/russross/blackfriday/v2"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 var (
@@ -21,17 +30,68 @@ var (
 
 // All the data that is needed for the template
 type Page struct {
-	Title   string
-	Content template.HTML
-	Image   string
-	Link    string
+	Title       string
+	Short       string
+	Image_url   string
+	Description string
+	Date        string
 }
 
 type Pages []Page
 
+var ps Pages
+
+func loadContent() {
+	ctx, cancel := context.WithTimeout(
+		context.Background(), 10*time.Second)
+	defer cancel()
+	opt := options.Client().ApplyURI("mongodb://127.0.0.1:21017/")
+	client, err := mongo.Connect(ctx, opt)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// check for connection
+	if err := client.Ping(ctx, readpref.Primary()); err != nil {
+		log.Fatal(err)
+	}
+
+	myProjects := client.Database("go_portfolio").Collection("projects")
+
+	// Find all projects
+	cursor, err := myProjects.Find(context.TODO(), bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Iterate through the cursor and print the documents
+	var projects []bson.M
+	if err = cursor.All(context.TODO(), &projects); err != nil {
+		log.Fatal(err)
+	}
+	for _, project := range projects {
+		page := Page{}
+		title, ok := project["title"]
+		short, ok := project["short"]
+		image_url, ok := project["image_url"]
+		description, ok := project["description"]
+		date, ok := project["date"]
+
+		if ok {
+			page.Title = title.(string)
+			page.Short = short.(string)
+			page.Image_url = image_url.(string)
+			page.Description = description.(string)
+			page.Date = date.(string)
+			ps = append(ps, page)
+		}
+	}
+}
+
 // Start the server and handle the requests
 // Sets the directory for static files
 func main() {
+
+	loadContent()
 
 	flag.Parse()
 	fs := http.FileServer(http.Dir("./static"))
@@ -52,23 +112,28 @@ func main() {
 // Here the index page is created with the index.templ.html
 func makeIndexHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		ps, err := loadPages(*srcDir)
+		// ps, err := loadPages()
+		// if err != nil {
+		// 	log.Println(err)
+		// }
+		err := renderPage(w, ps, "index.templ.html")
 		if err != nil {
 			log.Println(err)
 		}
-		err = renderPage(w, ps, "index.templ.html")
-		if err != nil {
-			log.Println(err)
-		}
+
 	}
 }
 
 // Here the project page is created with the project.templ.html
 func makePageHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		f := r.URL.Path[len("/projects/"):]
-		fpath := filepath.Join(*srcDir, f)
-		p, err := loadPage(fpath)
+
+		//get the url and split it to get the project name
+		url := r.URL.Path
+		url = url[10:]
+
+		p, err := loadPage(url)
+		fmt.Print(p)
 		if err != nil {
 			log.Println(err)
 		}
@@ -85,10 +150,12 @@ func renderPage(w io.Writer, data interface{}, content string) error {
 		filepath.Join(*tmpDir, "base.templ.html"),
 		filepath.Join(*tmpDir, content),
 	)
+
 	if err != nil {
 		return fmt.Errorf("renderPage.Parsefiles: %w", err)
 	}
 	err = temp.ExecuteTemplate(w, "base", data)
+
 	if err != nil {
 		return fmt.Errorf("renderPage.ExecuteTemplate: %w", err)
 	}
@@ -96,51 +163,22 @@ func renderPage(w io.Writer, data interface{}, content string) error {
 }
 
 // here the struct get filled with data
-func loadPage(fpath string) (Page, error) {
+func loadPage(url string) (Page, error) {
 	var p Page
-	fi, err := os.Stat(fpath)
-	if err != nil {
-		return p, fmt.Errorf("loadPage: %w", err)
+	fmt.Println(url)
+	fmt.Print(ps)
+
+	//search for the project in the struct
+	for _, page := range ps {
+		if page.Title == url {
+			p.Title = page.Title
+			p.Description = page.Description
+			p.Image_url = page.Image_url
+			p.Short = page.Short
+			p.Date = page.Date
+
+		}
 	}
-	p.Title = fi.Name()
-	b, err := ioutil.ReadFile(fpath)
-	if err != nil {
-		return p, fmt.Errorf("loadPage.ReadFile: %w", err)
-	}
-	p.Content = template.HTML(blackfriday.Run(b))
+
 	return p, nil
-}
-
-// here all apges are loaded and returned so it can be used in the index page
-func loadPages(src string) (Pages, error) {
-	var ps Pages
-	fs, err := ioutil.ReadDir(src)
-	if err != nil {
-		return ps, fmt.Errorf("loadPages.ReadDir: %w", err)
-	}
-	for _, f := range fs {
-		if f.IsDir() {
-			continue
-		}
-		fpath := filepath.Join(src, f.Name())
-		p, err := loadPage(fpath)
-		if err != nil {
-			return ps, fmt.Errorf("loadPages.loadPage: %w", err)
-		}
-		ps = append(ps, p)
-	}
-	return ps, nil
-}
-
-// a function that export all files in a directory
-func exportFiles(w http.ResponseWriter, r *http.Request) {
-	dir := "./projects"
-	files, err := ioutil.ReadDir(dir)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, file := range files {
-		http.ServeFile(w, r, dir+"/"+file.Name())
-	}
 }
